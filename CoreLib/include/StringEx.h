1eb7fccbaff878d8023a96a3f6757c630c92fe7e
sriiixi <subratoroy@hotmail.com>
2020-08-05 02:41:45 +0530

    ...

--- CoreLib/include/StringEx.h
+++ CoreLib/include/StringEx.h
@@ -32,6 +32,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <wchar.h>
 
 #if defined(_WIN32) || defined (WIN32) || defined (_WIN64)
 #define LIBRARY_EXPORT __declspec(dllexport)
